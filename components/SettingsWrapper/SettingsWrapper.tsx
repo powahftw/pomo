abc21ed26b5d0f37464373792d87fb3bc30b2806
Powah <federico.scloza@mail.polimi.it>
2022-02-24 23:25:31 +0100

    Used positioning library for Popovers

--- components/SettingsWrapper/SettingsWrapper.tsx
+++ components/SettingsWrapper/SettingsWrapper.tsx
@@ -1,11 +1,16 @@
 import { Settings } from 'react-feather';
-import React, { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
+import { Popover } from '@headlessui/react';
 import SettingsPicker from '../SettingsPicker';
 import Drawer from '../Drawer';
 import { DrawerDirection, DrawerTheme } from '../Drawer/Drawer';
+import { useFloating, offset, shift } from '@floating-ui/react-dom';
 
 export default function SettingsWrapper() {
+  const { x, y, reference, floating, strategy } = useFloating({
+    placement: 'bottom-end',
+    middleware: [offset(10), shift({ padding: 5 })],
+  });
+
   const button = (
     <Settings
       className="transition-colors duration-500 ease-in-out group-hover:animate-hop stroke-hc-color group-hover:stroke-hc-color-accent"
@@ -17,22 +22,20 @@ export default function SettingsWrapper() {
       {/* Desktop-like UI. */}
       <div className="hidden lg:block">
         <Popover className="relative">
-          <Popover.Button>{button}</Popover.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
+          <Popover.Button ref={reference}>{button}</Popover.Button>
+          <Popover.Panel
+            ref={floating}
+            style={{
+              position: strategy,
+              top: y ?? '',
+              left: x ?? '',
+            }}
+            className="z-10 w-60"
           >
-            <Popover.Panel className="absolute z-10 w-60 px-2 mt-3 transform sm:px-0 -right-2.5">
-              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white">
-                <SettingsPicker />
-              </div>
-            </Popover.Panel>
-          </Transition>
+            <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white">
+              <SettingsPicker />
+            </div>
+          </Popover.Panel>
         </Popover>
       </div>
       {/* Mobile-like UI. */}
